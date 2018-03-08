6d62e1f7a0d79f1ef487533f84e3604ccc7be6b2
Youping Li <>
2018-03-07 21:29:22 -0800

    finish PA-5

--- PA/Factorial_Iterative.asm
+++ PA/Factorial_Iterative.asm
@@ -9,21 +9,53 @@ charCR: .asciiz "\n"
 .globl main
 main:	print_str(msg1)
 	read_int($t0)
+	move $a0, $t0 		# move the argument into $a0
 	
 # Write body of the iterative
 # factorial program here
 # Store the factorial result into 
 # register $s0
-#
-# DON'T IMPLEMENT RECURSIVE ROUTINE 
-# WE NEED AN ITERATIVE IMPLEMENTATION 
-# RIGHT AT THIS POSITION. 
-# DONT USE 'jal' AS IN PROCEDURAL /
-# RECURSIVE IMPLEMENTATION.
+#------------------------------------------
+# Function: factorial
+# Argument: 
+#	$a0 : the number integrer number n
+# Returns:
+#	$v0 : factorial(n)
+# purpose: 
+#	To do the factorial in iteration manner
+#------------------------------------------
+fact:
+	# sp(8byte-double word boundary) + fp + ra + a0(argument) + s0(result) = 20-byte
+	addi	$sp, $sp, -20		# move $sp 
+	sw   	$fp, 20($sp)		# 
+	sw   	$ra, 16($sp)
+	sw   	$a0, 12($sp)
+	sw   	$s0,  8($sp)
+	addi 	$fp, $sp, 20
+	# preparing
+	move $t3, $a0			# t3 will be used to store product
+	addi $t2, $a0, -1		# t2 = t1 - 1
+	
+	# if t2 = 0 end iteration
+while:	blez $t2, end_while		# if t2 is less than or equal to 0 -> end
+	mulo $t3, $t3, $t2		# t3(n) = t3(n) * t2(n-1)
+	addi $t2, $t2, -1		# reduce t2 by 1
+	j	while			# jump to while
+	
+end_while:
+	move $s0, $t3			# store product into s0
+
 	
 	print_str(msg2)
 	print_reg_int($s0)
 	print_str(charCR)
+	
+	# restore all the saved registers
+	lw   	$fp, 20($sp)
+	lw   	$ra, 16($sp)
+	lw   	$a0, 12($sp)
+	lw   	$s0,  8($sp)
+	addi	$sp, $sp, 20
 	
 	exit
 	
